5c4378c8adbf2d1f450d227b274843fe45fd32f8
Braian de la Huerta Espinosa <bra.huerta20@gmail.com>
2026-01-21 23:28:58 -0600

    Update site URL in astro.config.mjs
    add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https//braianhuerta.github.io',
   vite: {
       plugins: [tailwindcss()],
   },
